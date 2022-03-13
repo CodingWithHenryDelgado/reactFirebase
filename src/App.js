48a018fbf5f0738c375930257b87e9a99a13537e
Henry_DelGlitch <CodingWHenry@gmail.com>
2022-03-12 19:35:48 -0500

    Implemented props and states.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import './Custom.css';
+import Card from './Card';
+const { faker } = require('@faker-js/faker');
 
 function App() {
+  const [name, setName] = useState('Alan Smith');
+  const [showCard, setShowCard] = useState(true);
+
+  const buttonsMarkup = (
+    <div>
+      <button className='button button2'>YES</button>
+      <button className='button button3'>NO</button>
+    </div>
+  );
+
+  const changeNameHandler = name => setName(name);
+
+  const changeInputHandler = event => setName(event.target.value);
+
+  const toggleShowCard = () => setShowCard(!showCard);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button className='button' onClick={toggleShowCard}>Toggle show/hide</button>
+      {showCard &&
+        <Card
+          avatar="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/127.jpg"
+          name={name}
+          title="Back End Developer"
+          onChangeName={() => changeNameHandler('Michael Chan')}
+          onChangeInput={changeInputHandler}
+        >{buttonsMarkup}
+        </Card>
+      }
     </div>
   );
 }
